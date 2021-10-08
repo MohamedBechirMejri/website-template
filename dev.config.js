3a3b4ab95997286bd7d5fd684b46be19de701733
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-10-08 12:40:09 +0100

    tweak babel config

--- dev.config.js
+++ dev.config.js
@@ -34,7 +34,12 @@ module.exports = {
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
     ],
   },
