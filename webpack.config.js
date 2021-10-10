f3b8ee96e0301e432eef9c6f0f1215eb93f852b2
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-10-10 16:45:56 +0100

    format code

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const {
-    CleanWebpackPlugin
-} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
